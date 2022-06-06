028da05125a3e43d331e24d7a04251ee7d778158
nll004 <loninath17@gmail.com>
2022-06-06 19:50:52 -0400

    restarted git tracking. previously was including secret api keys

---
+++ apis.py
@@ -0,0 +1,93 @@
+from requests import request
+from keys import rapidAPI_key
+
+import datetime
+import os.path
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# Code retrieved on 5/29/22 from GOOGLE API docs https://developers.google.com/calendar/api/quickstart/python.
+# Adapted to adjust parameters and results
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+def get_events():
+    """Shows basic usage of the Google Calendar API.
+    Prints the start and name of the next 10 events on the user's calendar.
+    """
+    creds = None
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build('calendar', 'v3', credentials=creds)
+
+        # Set start and endate (now and one week from now) to use as parameters in Calendar API
+        now = datetime.datetime.utcnow()
+        one_week = datetime.timedelta(days = 7)
+        next_week = now + one_week
+
+        # Convert both dates to strings
+        now_str = now.isoformat() + 'Z'  # 'Z' indicates UTC time
+        next_week_str = next_week.isoformat() + 'Z'
+
+        # Call the Calendar API
+        user_events = service.events().list(calendarId='primary', timeMin=now_str, timeMax=next_week_str,
+                                              singleEvents=True, orderBy='startTime').execute()
+        events = user_events.get('items', [])
+
+        if not events:
+            print('No upcoming events found.')
+            return
+
+        return events
+
+    except HttpError as error:
+        print('An error occurred: %s' % error)
+
+
+
+# 5/26/22. Code retrieved from: https://rapidapi.com/bitbiscuit-bitbiscuit-default/api/motivational-quotes1/
+def get_quote():
+    '''Get quote from rapidapi'''
+
+    base_url = "https://motivational-quotes1.p.rapidapi.com/motivation"
+
+    payload = {
+		"key1": "value",
+		"key2": "value"
+	}
+
+    headers = {
+		"content-type": "application/json",
+		"X-RapidAPI-Host": "motivational-quotes1.p.rapidapi.com",
+        "X-RapidAPI-Key": rapidAPI_key
+	}
+
+    response = request("POST", base_url, json=payload, headers=headers)
+
+    return response
+
+
+if __name__ == '__main__':
+    get_events()
+    get_quote()
---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template
+from apis import get_events, get_quote
+# from models import connect_db, db
+
+
+app = Flask(__name__)
+
+
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///tranquilist'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_ECHO'] = True
+
+
+# connect_db(app)
+
+@app.route('/')
+def display_home():
+	'''Display home for user'''
+
+	quote = get_quote()
+	calendar_events = get_events()
+
+	# for event in calendar_events:
+		# date = event['start']
+		# summary = event['summary']
+		# link = event['htmlLink']
+		# print('===================================Terminal:', start, summary, link)
+
+	return render_template('home.html', quote = quote, events = calendar_events)
